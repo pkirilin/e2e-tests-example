fd1bfb026525e5d9836a9d9112cb51dd28148c00
Pavel Kirilin <kirilin.pav@gmail.com>
2023-03-10 21:53:37 +0300

    Created client app

--- src/BackendApp.Web/Program.cs
+++ src/BackendApp.Web/Program.cs
@@ -4,6 +4,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "app/dist";
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -13,7 +18,23 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+app.UseStaticFiles();
+app.UseSpaStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(routes =>
+{
+    routes.MapControllers();
+});
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "app";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+    }
+});
 
 app.Run();
